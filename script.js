dea58c481a4792b69003594a63d475ca04351953
irshad-alungal <mialungal@gmail.com>
2023-09-20 17:07:37 +0530

    final v1

---
+++ script.js
@@ -0,0 +1,6 @@
+function showFrontLayer() {
+  document.getElementById("scroll").style.visibility = "visible";
+}
+function hideFrontLayer() {
+  document.getElementById("scroll").style.visibility = "hidden";
+}
